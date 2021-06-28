1d96bcedee6d9124a4dbdb778dbaafbf1a532a82
Your Name <you@example.com>
2021-06-28 23:44:55 +0900

    sh

--- shader/translate_fs.glsl
+++ shader/translate_fs.glsl
@@ -12,5 +12,7 @@ out vec4 fragColor;
 
 void main() {
   //209, 79, 65 ~ 67, 65, 209 Linear gradient
-  fragColor = vec4( (vec3(0.255​,0.82,0.263)-vec3(0.82,0.31,0.255))*(val-1.0)+vec3(0.82,0.31,0.255), 1.0)
+  vec3 red = vec3(0.82,0.31,0.255)
+  vec3 blue = vec3(0.255​,0.82,0.263)
+  fragColor = vec4( (blue-red)*(val-1.0)+red, 1.0)
 }
