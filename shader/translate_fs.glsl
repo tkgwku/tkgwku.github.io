c406113eada6db9bf8b4390958ac7d399d685eef
Your Name <you@example.com>
2021-06-29 00:20:01 +0900

    sh

--- shader/translate_fs.glsl
+++ shader/translate_fs.glsl
@@ -6,13 +6,8 @@
 
 precision highp float;
 
-in float val;
-
-out vec4 fragColor;
+varying float vColor;
 
 void main() {
-  //209, 79, 65 ~ 67, 65, 209 Linear gradient
-  vec3 red = vec3(0.82,0.31,0.255);
-  vec3 blue = vec3(0.255​,0.82,0.263);
-  fragColor = vec4( (blue-red)*(val-1.0)+red, 1.0);
+  gl_FragColor = vColor;
 }
